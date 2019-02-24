468e0fd2b60a84ef87470f7581809a8e1046a57f
Alexis Dominguez <alexis@Alexiss-MacBook-Pro.local>
2019-02-24 16:47:12 -0500

    Update MusicOrganizer.java
    4.43: which allows the user to play one random song.

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -1,124 +1,125 @@
-import java.util.ArrayList;
-
-/**
- * A class to hold details of audio tracks.
- * Individual tracks may be played.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29
- */
-public class MusicOrganizer
-{
-    // An ArrayList for storing music tracks.
-    private ArrayList<Track> tracks;
-    // A player for the music tracks.
-    private MusicPlayer player;
-    // A reader that can read music files and load them as tracks.
-    private TrackReader reader;
-
-    /**
-     * Create a MusicOrganizer
-     */
-    public MusicOrganizer()
-    {
-        tracks = new ArrayList<>();
-        player = new MusicPlayer();
-        reader = new TrackReader();
-        readLibrary("../audio");
-        System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
-        System.out.println();
-    }
+    import java.util.ArrayList;
+    import java.util.Random;
     
     /**
-     * Add a track file to the collection.
-     * @param filename The file name of the track to be added.
+     * A class to hold details of audio tracks.
+     * Individual tracks may be played.
+     * 
+     * @author David J. Barnes and Michael Kölling
+     * @version 2016.02.29
      */
-    public void addFile(String filename)
+    public class MusicOrganizer
     {
-        tracks.add(new Track(filename));
-    }
+        // An ArrayList for storing music tracks.
+        private ArrayList<Track> tracks;
+        // A player for the music tracks.
+        private MusicPlayer player;
+        // A reader that can read music files and load them as tracks.
+        private TrackReader reader;
     
-    /**
-     * Add a track to the collection.
-     * @param track The track to be added.
-     */
-    public void addTrack(Track track)
-    {
-        tracks.add(track);
-    }
-    
-    /**
-     * Play a track in the collection.
-     * @param index The index of the track to be played.
-     */
-    public void playTrack(int index)
-    {
-        if(indexValid(index)) {
-            Track track = tracks.get(index);
-            player.startPlaying(track.getFilename());
-            System.out.println("Now playing: " + track.getArtist() + " - " + track.getTitle());
+        /**
+         * Create a MusicOrganizer
+         */
+        public MusicOrganizer()
+        {
+            tracks = new ArrayList<>();
+            player = new MusicPlayer();
+            reader = new TrackReader();
+            readLibrary("../audio");
+            System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
+            System.out.println();
         }
-    }
-    
-    /**
-     * Return the number of tracks in the collection.
-     * @return The number of tracks in the collection.
-     */
-    public int getNumberOfTracks()
-    {
-        return tracks.size();
-    }
-    
-    /**
-     * List a track from the collection.
-     * @param index The index of the track to be listed.
-     */
-    public void listTrack(int index)
-    {
-        System.out.print("Track " + index + ": ");
-        Track track = tracks.get(index);
-        System.out.println(track.getDetails());
-    }
-    
-    /**
-     * Show a list of all the tracks in the collection.
-     */
-    public void listAllTracks()
-    {
-        System.out.println("Track listing: ");
-
-        for(Track track : tracks) {
-            System.out.println(track.getDetails());
+        
+        /**
+         * Add a track file to the collection.
+         * @param filename The file name of the track to be added.
+         */
+        public void addFile(String filename)
+        {
+            tracks.add(new Track(filename));
         }
-        System.out.println();
-    }
-    
-    /**
-     * List all tracks by the given artist.
-     * @param artist The artist's name.
-     */
-    public void listByArtist(String artist)
-    {
-        for(Track track : tracks) {
-            if(track.getArtist().contains(artist)) {
-                System.out.println(track.getDetails());
+        
+        /**
+         * Add a track to the collection.
+         * @param track The track to be added.
+         */
+        public void addTrack(Track track)
+        {
+            tracks.add(track);
+        }
+        
+        /**
+         * Play a track in the collection.
+         * @param index The index of the track to be played.
+         */
+        public void playTrack(int index)
+        {
+            if(indexValid(index)) {
+                Track track = tracks.get(index);
+                player.startPlaying(track.getFilename());
+                System.out.println("Now playing: " + track.getArtist() + " - " + track.getTitle());
             }
         }
-    }
-    
-    /**
-     * Remove a track from the collection.
-     * @param index The index of the track to be removed.
-     */
-    public void removeTrack(int index)
-    {
-        if(indexValid(index)) {
-            tracks.remove(index);
+        
+        /**
+         * Return the number of tracks in the collection.
+         * @return The number of tracks in the collection.
+         */
+        public int getNumberOfTracks()
+        {
+            return tracks.size();
         }
-    }
+        
+        /**
+         * List a track from the collection.
+         * @param index The index of the track to be listed.
+         */
+        public void listTrack(int index)
+        {
+            System.out.print("Track " + index + ": ");
+            Track track = tracks.get(index);
+            System.out.println(track.getDetails());
+        }
+        
+        /**
+         * Show a list of all the tracks in the collection.
+         */
+        public void listAllTracks()
+        {
+            System.out.println("Track listing: ");
     
-    /**
-     * Play the first track in the collection, if there is one.
+            for(Track track : tracks) {
+                System.out.println(track.getDetails());
+            }
+            System.out.println();
+        }
+        
+        /**
+         * List all tracks by the given artist.
+         * @param artist The artist's name.
+         */
+        public void listByArtist(String artist)
+        {
+            for(Track track : tracks) {
+                if(track.getArtist().contains(artist)) {
+                    System.out.println(track.getDetails());
+                }
+            }
+        }
+        
+        /**
+         * Remove a track from the collection.
+         * @param index The index of the track to be removed.
+         */
+        public void removeTrack(int index)
+        {
+            if(indexValid(index)) {
+                tracks.remove(index);
+            }
+        }
+        
+        /**
+         * Play the first track in the collection, if there is one.
      */
     public void playFirst()
     {
@@ -134,6 +135,17 @@ public class MusicOrganizer
     {
         player.stop();
     }
+    
+     public void randomPlay()
+     {
+         if(tracks.size() > 0) {
+             Random rand = new Random();
+             int index = rand.nextInt(tracks.size());
+             playTrack(index);
+            }
+     }
+     
+     
 
     /**
      * Determine whether the given index is valid for the collection.
